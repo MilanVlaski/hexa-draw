e2e6049d2488096937b9a33676737c93df3e7c54
Milan Vlaski <milan.vlaski321@gmail.com>
2025-06-22 13:44:27 +0200

    fix: serialization test writes files to temp directory

--- app/src/test/java/com/mutaki/hexadraw/SerializationTest.java
+++ app/src/test/java/com/mutaki/hexadraw/SerializationTest.java
@@ -1,16 +1,18 @@
 package com.mutaki.hexadraw;
 
-import java.awt.Point;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
-
 import com.mutaki.hexadraw.io.json.JsonCircuitFileReader;
 import com.mutaki.hexadraw.io.json.JsonCircuitFileWriter;
 import com.mutaki.hexadraw.model.Circuit;
 import com.mutaki.hexadraw.model.JunctionBox;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SerializationTest {
 
@@ -19,11 +21,13 @@ public class SerializationTest {
 
     @Test
     void Object_from_model_to_document_back_to_model() throws IOException {
-        Path saveDirectory = Path.of(""); // Files.createTempDirectory("temp");
+        // Use Path.of(""); if debugging
+        Path saveDirectory =  Files.createTempDirectory("temp");
         var circuit = new Circuit(name, List.of(new JunctionBox(point)));
         new JsonCircuitFileWriter(circuit).write(saveDirectory);
 
         Path circuitFilePath = saveDirectory.resolve(name + ".json");
         var writeCircuit = new JsonCircuitFileReader(circuitFilePath).read();
+        assertTrue(writeCircuit.has(JunctionBox.class));
     }
 }
