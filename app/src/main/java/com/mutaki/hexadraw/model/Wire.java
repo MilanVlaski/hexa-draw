2f73bc6de665aff1ec57bad8397f0d4320841ad4
Milan Vlaski <milan.vlaski321@gmail.com>
2025-06-28 13:14:24 +0200

    feat: change error message

--- app/src/main/java/com/mutaki/hexadraw/model/Wire.java
+++ app/src/main/java/com/mutaki/hexadraw/model/Wire.java
@@ -12,7 +12,7 @@ public class Wire implements Element {
 
     public Wire(JunctionPoint junctionPoint1, JunctionPoint junctionPoint2) {
         if (junctionPoint1.equals(junctionPoint2)) {
-            throw new IllegalArgumentException("JunctionPoints must not be equal");
+            throw new IllegalArgumentException("Can't connected two of the same element.");
         }
         this.junctionPoint1 = junctionPoint1;
         this.junctionPoint2 = junctionPoint2;
